158014ce3b2a4accae5dd86c9acaf7fbe889da32
roushan-sheik <roushansheik@gmail.com>
2025-07-28 19:41:08 +0600

    Add user route and hot user reloader

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -6,5 +6,14 @@ app = Flask(__name__)
 def home():
     return 'Hello, Flask App is running By Docker compose'
 
+@app.route('/users')
+def get_users():
+    users = [
+        {'id': 1, 'name': 'Alice', 'age':25},
+        {'id': 2, 'name': 'Bob', 'age':29},
+        {'id': 3, 'name': 'Charlie', 'age':20}
+    ]
+    return jsonify(users)
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0',  use_reloader=True)
